7371ad806373c3441646a3dc356c6d1f1158772b
klaus <keylmann@gmail.com>
2016-12-26 18:11:58 +0100

    added non-tracked files

---
+++ app/src/main/java/com/example/klaus/test_project_12262016/Project_12_2016.java
@@ -0,0 +1,13 @@
+package com.example.klaus.test_project_12262016;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Project_12_2016 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_project_12_2016);
+    }
+}
